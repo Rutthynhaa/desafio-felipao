46f69ca5ea5504960caf196a74811b5af207bbe9
Rutthynhaa <ruth_ellen.araujo@hotmail.com>
2024-05-23 10:03:32 -0300

    DEsafio Felipao Dio

--- src/index.js
+++ src/index.js
@@ -4,33 +4,31 @@ let xpHeroi = 9000
 
 //Utilizar uma estrutura de decisão para apresentar a mensagem contendo o nível do Heroi. 
 //Usei a estrutura condicional switch case.
+let nivelHeroi;
 switch (xpHeroi) {
-    case (xpHeroi < 1000):
-     Console.log ("Ferro");
+    case xpHeroi < 1000:
+     nivelHeroi = "Ferro";
      break;
-    case (xpHeroi >= 1001 && xpHeroi <= 2000):
-     console.log("Bronze");
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+     nivelHeroi = "Bronze";
      break;
-    case (xpHeroi >= 2001 && xpHeroi <= 5000):
-     console.log("Prata");
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+     nivelHeroi = "Prata";
      break;
-    case (xpHeroi >= 5001 && xpHeroi <= 7000):
-     console.log("Ouro");
+    case xpHeroi >= 5001 && xpHeroi <= 7000:
+     nivelHeroi = "Ouro";
      break;
-    case (xpHeroi >= 7001 && xpHeroi <= 8000):
-     console.log("Platina");
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+     nivelHeroi = "Platina";
      break;
-    case (xpHeroi >= 8001 && xpHeroi <= 9000):
-     console.log("Ascendente")
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+     nivelHeroi = "Ascendente"
      break;
-    case (xpHeroi >= 9001 && xpHeroi <= 10000):
-     console.log("Imortal");
-     break;
-    case (xpHeroi >= 10001 ):
-     console.log("Radiante");
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+     nivelHeroi = "Imortal";
      break;
     default:
-     console.log("Xp inválido") 
+     nivelHeroi = "Radiante"
 
 }
-    Console.log("O Herói " + nomeHeroi + "está no nível " + xpHeroi)
+    console.log("O Herói " + nomeHeroi + " " + "está no nível " + xpHeroi)
